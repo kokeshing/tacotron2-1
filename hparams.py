702a28ecd65204ee8c6bc2052fd6f41e92c22e55
kokeshing <kou7cdb1a@gmail.com>
2020-04-30 02:27:20 +0900

    change hparams

--- hparams.py
+++ hparams.py
@@ -39,7 +39,7 @@ def create_hparams(hparams_string=None, verbose=False):
         win_length=1024,
         n_mel_channels=80,
         mel_fmin=0.0,
-        mel_fmax=8000.0,
+        mel_fmax=None,
 
         ################################
         # Model Parameters             #
